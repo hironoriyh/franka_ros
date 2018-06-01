789022ddaca8565f7b4419be03c15ac966bb23e3
hyoshida <hyoshida@preferred.jp>
2018-06-01 10:42:06 +0900

    testing

--- franka_example_controllers/scripts/move_to_start.py
+++ franka_example_controllers/scripts/move_to_start.py
@@ -7,6 +7,13 @@ from actionlib_msgs.msg import GoalStatusArray
 if __name__ == '__main__':
     rospy.init_node('move_to_start')
     rospy.wait_for_message('move_group/status', GoalStatusArray)
+    # rospy.logwarn(GoalStatusArray.status_list[0].goal_id)
+    # print GoalStatusArray
     commander = MoveGroupCommander('panda_arm')
     commander.set_named_target('ready')
+    # rospy.logwarn(commander.goal_id)
     commander.go()
+
+    # rospy.Rate(2).sleep()
+    # commander.set_named_target('home')
+    # commander.go()
---
+++ franka_example_controllers/scripts/moveit.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import moveit_commander
+# from moveit_commander import MoveGroupCommander
+
+import rospy
+import geometry_msgs.msg
+
+
+def main():
+	rospy.init_node("moveit_test")
+	r = rospy.Rate(10) # 10hz
+
+	robot = moveit_commander.RobotCommander()
+	group = moveit_commander.MoveGroupCommander('panda_arm')
+	group.clear_pose_targets()
+	# rospy.Subscriber("chatter", String, callback)
+
+	cnt = 0
+	# while not rospy.is_shutdown():
+	# while cnt <3:
+	cnt = cnt +1
+	r.sleep()
+	joints = group.get_current_joint_values()
+	print (joints[2], group.get_name())
+	joints[2] = joints[2] + 0.5
+	group.set_joint_value_target(joints)
+	group.go()
+
+	rospy.Rate(0.5).sleep()
+
+	joints[2] = joints[2] - 1.0
+	group.set_joint_value_target(joints)
+	group.go()
+
+	rospy.Rate(0.5).sleep()
+	#
+	# joints[2] = joints[2] + 0.5
+	# group.set_joint_value_target(joints)
+	# group.go()
+
+
+if __name__ == '__main__':
+	# try:
+    main()
+	# except rospy.ROSInterruptException:
+	# 	pass
